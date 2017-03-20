e14cd9cb81bd77a034de03663f3048d4ff668498
Annija <annijamilere@gmail.com>
2017-03-20 12:32:14 +0200

    Kods

---
+++ Assets/MainMenuControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControler : MonoBehaviour {
+
+	public void Play()
+    {
+        Debug.Log("play pressed");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quit pressed");
+        Application.Quit();
+    }
+}
